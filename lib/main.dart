d1ffc404cb6ebe1abae6066a307ab0e3ab0fcbd7
Nisarg Ratani <nisarg.r@soultelabs.com>
2021-04-06 10:20:44 +0530

    Use of FutureProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/all.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
-final greetingProvider = Provider((ref) => 'Hello Riverpod!');
-final secondProvider = Provider((ref) => 'This is Second');
-
-class IncrementNotifier extends ChangeNotifier {
-  int _value = 0;
-  int get value => _value;
-
-  void increment() {
-    _value += 1;
-    notifyListeners();
+class FakeHttpClient {
+  Future<String> get(String url) async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 'Response from $url';
   }
 }
 
-final incrementProvider = ChangeNotifierProvider((ref) => IncrementNotifier());
+final fakeHttpClientProvider = Provider((ref) => FakeHttpClient());
+final responseProvider =
+    FutureProvider.autoDispose.family<String, String>((ref, url) async {
+  final httpClient = ref.read(fakeHttpClientProvider);
+  return httpClient.get(url);
+});
 
 class MyApp extends StatelessWidget {
   @override
@@ -32,31 +31,20 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Riverpod'),
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            context.read(incrementProvider).increment();
+        body: Consumer(
+          builder: (context, watch, child) {
+            final response = watch(
+              responseProvider('https://www.google.com'),
+            );
+            return response.map(
+              data: (_) => Center(child: Text(_.value)),
+              loading: (_) => Center(child: CircularProgressIndicator()),
+              error: (_) => Text(
+                'Error occurs',
+                style: TextStyle(color: Colors.red),
+              ),
+            );
           },
-        ),
-        body: Center(
-          child: Consumer(
-            builder: (context, watch, child) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    watch(incrementProvider).value.toString(),
-                  ),
-                  Text(
-                    watch(greetingProvider),
-                  ),
-                  Text(
-                    watch(secondProvider),
-                  ),
-                ],
-              );
-            },
-          ),
         ),
       ),
     );
